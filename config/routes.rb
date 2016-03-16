515ffd488cb87233f53aadc647dee3b5a59ca7a0
Chelsea Johnson <cheljoh@gmail.com>
2016-03-16 17:41:35 -0600

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'signup'  => 'users#new'
+  get 'sessions/new'
 
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  root             'static_pages#home'
-  get 'contact' => 'static_pages#contact'
+  get 'signup'     => 'users#new'
+
+  get 'help'       => 'static_pages#help'
+  get 'about'      => 'static_pages#about'
+  root                'static_pages#home'
+  get 'contact'    => 'static_pages#contact'
+  get 'login'      => 'sessions#new'
+  post 'login'     => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
