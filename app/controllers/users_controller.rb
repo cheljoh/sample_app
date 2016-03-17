e8ab22d3109d9084139d6d2045ad25377e71c4df
Chelsea Johnson <cheljoh@gmail.com>
2016-03-17 10:42:35 -0600

    finish ch 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def new
   end
@@ -37,6 +43,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+
 private
 
   def user_params
@@ -45,8 +57,23 @@ private
 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in"
       redirect_to login_path
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
+  end
+
+  def admin_user
+    if current_user.nil?
+      redirect_to login_path
+    elsif !current_user.admin?
+      redirect_to login_path
+    end
+    # redirect_to(root_url) unless current_user.admin?
   end
 end
