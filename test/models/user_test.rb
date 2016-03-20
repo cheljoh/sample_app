e74eafb34f99babc931a20b0e8e8b3e9cae97aff
Chelsea Johnson <cheljoh@gmail.com>
2016-03-19 23:10:23 -0600

    work on ch.12

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,4 +70,15 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    refute michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    refute michael.following?(archer)
+  end
+
 end
